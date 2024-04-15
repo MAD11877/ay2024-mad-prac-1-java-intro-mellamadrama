34d5d6c5f8e19f79ec245fb290d8ad45a9fe9f4a
mellamadrama <s10258027@connect.np.edu.sg>
2024-04-15 14:41:35 +0800

    q2

--- Question2.java
+++ Question2.java
@@ -4,13 +4,15 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Input weight in kg: ");
+    double weight = in.nextDouble();
+
+    System.out.print("Input height in cm: ");
     double height = in.nextDouble();
+
+    double BMI = weight / ((height / 100) * (height / 100));
+    System.out.print("Body Mass Index is " + BMI+"\n");
   }
 }
